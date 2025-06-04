483e55a07dd3a2531a8ab9e8d9982ac91f44e07f
YashPatankar483 <yashpatankar483@gmail.com>
2025-06-04 21:19:37 +0530

    Day 1: Solved right angled number triangle pattern problem

---
+++ rt_angledNumTri.py
@@ -0,0 +1,18 @@
+
+print("________________________________________________________")
+print("Right Angled Number Triangle")
+print("________________________________________________________")
+
+while True:
+    height = int(input("Enter the height (positive value) of the triangle: "))
+    if height > 0:
+        break
+
+print("________________________________________________________")
+
+for i in range(0, height):
+    for j in range(0, i+1):
+        print(j+1, end=" ")
+    print("")
+
+print("________________________________________________________")
