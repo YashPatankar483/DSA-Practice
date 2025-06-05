94cbc43caa63abf237510da56f6f91fd6e34ff9a
YashPatankar483 <yashpatankar483@gmail.com>
2025-06-05 23:48:12 +0530

    Day2: Count number of digits in a given number

---
+++ digit_count.py
@@ -0,0 +1,11 @@
+# Problem Statement:  Given an integer N, return the number of digits in N.
+
+
+num = int(input("Enter any number: "))
+count = 0
+
+while num > 0:
+    num = num % 10
+    count += 1
+
+print(count)
